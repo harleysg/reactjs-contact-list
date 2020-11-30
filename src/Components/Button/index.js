75e7b2e2e8a940abf3190f7744ecde7c6dedcf9e
Harley Santos Garzon <harley.santos@globant.com>
2020-11-30 17:52:12 -0500

    refactor(Components/Button): Manage icon and styles
    Include Icons components
    refactor styles components

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { string as isString } from "prop-types"
+
+import { Add, Close, Love, Trash } from "../Icons";
 import ButtonStyled from "./styles";
+
+function FilterIcon(name){
+  switch (name) {
+    case "love":
+      return <Love />
+    case "add":
+      return <Add />
+    case "close":
+      return <Close />
+    case "trash":
+      return <Trash />
+    default:
+      return null
+  }
+}
 
 export default function Button({ icon, label, ...others }) {
   return (
     <ButtonStyled {...others}>
-      <span>{label}</span>
+      {icon ? FilterIcon(icon) : null}
+      {label ? <span>{label}</span> : null}
     </ButtonStyled>
   );
+}
+
+Button.propTypes = {
+  icon: isString,
+  label: isString,
+  rtl: isString,
+  theme: isString,
 }
--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -6,6 +6,7 @@ function useStatus(value) {
     case "solid":
       return (theme = `
         background-color: var(--color_primary);
+        color: white;
       `);
     case "disabled":
       return (theme = ``);
@@ -22,14 +23,29 @@ function useStatus(value) {
 
 const ButtonStyled = styled.button((props) => {
   return `
-    display: grid;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: ${props.rtl ? "row-reverse": "row"};
     gap: 0.3rem;
+    
     border-radius: 6px;
-    padding: 0.1rem 0.5rem;
+    padding: 0.2rem 0.5rem;
+    min-height: 2rem;
+    
     background-color: var(--theme_bg_12);
     color: var(--theme_color);
     ${useStatus(props.theme)}
     cursor: pointer;
+    text-transform: uppercase;
+    font-size: 0.7rem;
+
+    span {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+    }
   `;
 });
 
