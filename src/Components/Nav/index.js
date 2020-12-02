fc0e983e4b0f957eafe7c2cbcb15eb8d6ef08390
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-12-01 23:57:18 -0500

    feat(*): new Form component
    Create Form component and include on
    Routes.js
    Se agrega prop en Nav component para evento

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { func as isFunc } from "prop-types"
 
 import { Menu } from "./styles";
 
 import MenuItem from "./NavItem";
 
-export default function Nav() {
+export default function Nav({action}) {
   return (
     <Menu>
       <MenuItem exact text="Overview" type="link" to="/"/>
       <MenuItem exact text="Contact" type="link" to="/contact"/>
       <MenuItem exact text="Favorites" type="link" to="/favorites"/>
-      <MenuItem text="new" type="button" theme="solid" icon="add" event={() => console.log(`.....`)}/>
+      <MenuItem text="new" type="button" theme="solid" icon="add" event={() => action()}/>
     </Menu>
   );
+}
+
+Nav.propTypes = {
+  action: isFunc.isRequired
 }
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,18 +4,20 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Overview from "../Pages/Overview";
 import { Contact } from "../Pages/Contact";
 import { Favorites } from "../Pages/Favorites";
-import { Header, Nav, Wrapper, Brand } from "../Components";
+import { Header, Nav, Wrapper, Brand, Form } from "../Components";
+import { useShowForm } from "../Hooks";
 
 export default function Routes() {
+  const { show, setShow } = useShowForm()
   return (
     <BrowserRouter>
       <Header>
         <Wrapper space="2" flex alignItems="center" justifyContent="space-between">
           <Brand size={3} />
-          <Nav />
+          <Nav action={() => setShow(!show)}/>
         </Wrapper>
       </Header>
-
+      {show && <Form />}
       <Switch>
         <Route path="/" exact>
           <Overview />
