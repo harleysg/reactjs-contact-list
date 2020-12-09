b9c74491c71adc01cbefa6ec638afa669f929d1d
Harley Santos Garzon <harley.santos@globant.com>
2020-12-09 15:04:39 -0500

    refactor(Pages/*): Include PAgination component

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux"
-import { CardList, Heading, Pagination, Section, Wrapper } from "../Components";
+import { Heading, Pagination, Section, Wrapper } from "../Components";
 
 export function Contact() {
   const contacts = useSelector(state => state.contacts.all)
@@ -8,10 +8,12 @@ export function Contact() {
     <Wrapper space="2">
       <Heading title="Contacts" bg="var(--color_primary)" />
       <Section>
-        <CardList data={contacts} field="contacts_list"/>
-      </Section>
-      <Section>
-        <Pagination />
+        <Pagination
+          field="contacts_list"
+          data={contacts}
+          itemsPerPage={8}
+          startFrom={1}
+        />
       </Section>
     </Wrapper>
   );
--- src/Pages/Favorites.js
+++ src/Pages/Favorites.js
@@ -1,17 +1,20 @@
 import { useSelector } from "react-redux"
 
-import { CardList, Heading, Pagination,Section, Wrapper } from "../Components";
+import { Pagination, Heading, Section, Wrapper } from "../Components";
 
 export function Favorites() {
   const contacts = useSelector(state => state.contacts.favorites)
+  
   return (
     <Wrapper space="2">
       <Heading title="Favorites" bg="var(--color_primary)" />
       <Section>
-        <CardList data={contacts} field="favorites"/>
-      </Section>
-      <Section>
-        <Pagination />
+        <Pagination
+          field="favorites"
+          data={contacts}
+          itemsPerPage={6}
+          startFrom={1}
+        />
       </Section>
     </Wrapper>
   );
