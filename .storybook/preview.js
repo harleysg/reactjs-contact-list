d3bc12384f0720e5ab91ceea7d578fc08ba62e8e
Harley Santos Garzón <harley.santos@globant.com>
2020-12-18 12:03:01 -0500

    refactor(.storybook/Preview):  decoradores
    Se agregan decorador para manejo de depencendias de rutas y estilos

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+import React from "react";
+
+import { MemoryRouter } from 'react-router-dom';
+import { addDecorator } from "@storybook/react";
+
+import Styles from "../src/assets/styles";
+
+addDecorator(story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>);
+addDecorator(story => <MemoryRouter initialEntries={['/contacts']}>{story()}</MemoryRouter>);
+addDecorator(story => <MemoryRouter initialEntries={['/favorites']}>{story()}</MemoryRouter>);
+addDecorator(story => <><Styles />{story()}</>); // Set globalStyles into StoryBooks
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
